e3ce6740835f5c796e8aa822f327fe84018e43e6
judsoncabral <judsoncabral1@hotmail.com>
2020-11-08 16:12:36 -0300

    Adicionando menu e icones

---
+++ app/src/main/java/com/example/gestordecusto/activity/InformacaoActivity.kt
@@ -0,0 +1,11 @@
+package com.example.gestordecusto.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class InformacaoActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_informacao)
+    }
+}
